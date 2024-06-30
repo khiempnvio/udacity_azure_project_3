4a42e0f101ddfb5a3f352269a761074cae967217
khiempn <khiempn@hotmail.com>
2024-06-30 12:05:15 +0700

    add ARM_ACCESS_KEY

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -2,8 +2,7 @@
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
-variable "tenant_id" {}
-variable "access_key" {}
+variable "tenant_id" {} 
 
 # Resource Group/Location
 variable "location" {}
