e6f03e38931810add9f7c0587b5e7707695d358d
khiempn <khiempn@hotmail.com>
2024-06-30 15:45:55 +0700

    update disable_password_authentication

--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -7,9 +7,9 @@ tenant_id = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 # Resource Group/Location
 location = "East US"
 resource_group = "Azuredevops"
-application_type = "myApplication"
+application_type = "myTerraform"
 
 # Network
-virtual_network_name = "myApplication_virtual_network_name"
+virtual_network_name = "myTerraform_virtual_network_name"
 address_space = "10.5.0.0/16"
 address_prefix_test = "10.5.1.0/24"
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -18,6 +18,13 @@ resource "azurerm_linux_virtual_machine" "project_3" {
   size                = "Standard_DS2_v2"
   admin_username      = "devopsagent"
   admin_password      = "DevOpsAgent@123"
+  disable_password_authentication = false
+  
+  /*admin_ssh_key {
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkvLEVD436vyghNnQEYz/CPddchLmY+iUyJz/YhpXCpC93Um9OTZ4Z7AF9PYy5Nrzit0jSu0bc5hlhIYLSSTgxOntctBJo23IWwWut8q/NFbjAaL+5HrfcNIXLt4nK/wPkakzT68/vg/97eWI/H6ghFxFc3cW3pSfTGILsZKryR0QXPv6okVQAOZfJWhnx6QNv82B5TpPW75sxK304Q6h23YJXhH+gd+ZKSVhaKU/cc/SfRJ2vrneqSkM/IjZGiu7+Myvil1tlTVAtoso1vMEOpt/iz6x9ni8GZ2Iw2VUNbuu+bY/aI//7rWC6/Z1ZZpv6E3cWm+uSLuSSORuVqr9NcvyT346GKONNWWnv0octTxRy5/D/1U+xgng1btn7Hp7zwri+FKd5SV4DdR+J6BHJZY1JgPh5PRsDUCsRglsYyeypMJRsHcx0ZCl+0DC7T1KZV8XK5XTUd8VTgB4NJopcYIGIhqfIlxjVWDrZECwE7thnozEAyigQMJoNSe6rtRU= KHIEM@KHIEMPN"
+  }*/
+
   network_interface_ids = [azurerm_network_interface.project_3.id]
   os_disk {
     caching           = "ReadWrite"
