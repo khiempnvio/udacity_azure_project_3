d8708084d08d4a08d3264e4618678ebd295923cc
khiempn <khiempn@hotmail.com>
2024-06-30 15:18:39 +0700

    try again

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -31,38 +31,3 @@ module "network" {
   resource_group       = "${var.resource_group}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
-
-module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
-  resource_type    = "NSG"
-  resource_group   = "${var.resource_group}"
-  subnet_id        = "${module.network.subnet_id_test}"
-  address_prefix_test = "${var.address_prefix_test}"
-}
-module "appservice" {
-  source           = "../../modules/appservice"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
-  resource_type    = "AppService"
-  resource_group   = "${var.resource_group}"
-}
-
-module "publicip" {
-  source           = "../../modules/publicip"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
-  resource_type    = "publicip"
-  resource_group   = "${var.resource_group}"
-}
-
-module "vm" {
-  source           = "../../modules/vm"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
-  resource_type    = "VirtualMachine"
-  resource_group   = "${var.resource_group}"
-  subnet_id        = "${module.network.subnet_id_test}"
-  public_ip_address_id        = "${module.publicip.public_ip_address_id}"
-}
